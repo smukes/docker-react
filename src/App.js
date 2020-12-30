205ed750dac0846cb2d6c8c700f04dc830bbd0df
Mukeshraj Sankararaj <smukeshraj96@hotmail.com>
2020-12-30 20:59:23 +0530

    Added Mukesh Rocks

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         DOCKER ROCKS
+         MUKESH ROCKS
         </p>
         <a
           className="App-link"
